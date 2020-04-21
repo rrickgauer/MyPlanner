fcd6a5d456c5d2cd26c6c091fe9e3d2cb5db4406
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-04-21 15:44:15 -0500

    make index.php

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>My Planner</title>
+   </head>
+   <body>
+      <h1>My Planner</h1>
+      <p>Written by Ryan Rickgauer</p>
+   </body>
+</html>

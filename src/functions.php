c39e21e636e44821c3e76cdd616d802a0dc83450
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-04-21 16:21:41 -0500

    add insertUser()

--- src/functions.php
+++ src/functions.php
@@ -15,4 +15,31 @@ function dbConnect() {
   }
 }
 
+function insertUser($email, $password) {
+
+   // connect to db
+   $pdo = dbConnect();
+
+   // prepare sql statement
+   $sql = $pdo->prepare('INSERT INTO Users (email, password) VALUES (:email, :password)');
+
+   // filter, sanitize, and hash email and password
+   $email    = filter_var($email, FILTER_SANITIZE_EMAIL);
+   $password = filter_var($password, FILTER_SANITIZE_STRING);
+   $password = password_hash($password, PASSWORD_DEFAULT);
+
+   // bind parameters to the prepared sql statement
+   $sql->bindParam(':email', $email, PDO::PARAM_STR);
+   $sql->bindParam(':password', $password, PDO::PARAM_STR);
+
+   // execute statement and close the connections
+   $sql->execute();
+   $sql = null;
+   $pdo = null;
+}
+
+
+
+
+
 ?>

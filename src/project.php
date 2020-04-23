7511fc5e41475b6ccd016e902a175a9fb8569976
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-04-23 17:19:51 -0500

    add sass and sidebar

--- src/project.php
+++ src/project.php
@@ -10,30 +10,34 @@
 <body>
   <?php include('navbar.php'); ?>
 
-  <div class="container">
+  <div class="wrapper">
 
-    <h1>Project</h1>
+    <!-- put project checklists here -->
+    <nav id="sidebar">
+      <ul>
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+        <li><a href="#">Page 4</a></li>      
+      </ul>
+    </nav>
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <div id="content">
+      <div class="container-fluid">
+        <button type="button" class="btn btn-primary" onclick="activateSidebar()">View checklists</button>
+      </div>
+    </div>
 
   </div>
+
   
   <?php include('footer.php'); ?>
+
+  <script>
+    function activateSidebar() {
+      $('#sidebar').toggleClass('active');
+    }
+  </script>
+
 </body>
 </html>

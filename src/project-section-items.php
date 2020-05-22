f00c14248181c3171c8d0c33fff1db258cf813bb
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-05-22 14:13:21 -0500

    merge all pills into one page

--- src/project-section-items.php
+++ src/project-section-items.php
@@ -47,171 +47,156 @@
       <!-- body -->
       <div class="modal-body">
 
-        <!-- item modal navbar -->
-        <ul class="nav nav-pills justify-content-center" role="tablist">
-          <li class="nav-item">
-            <a class="nav-link" id="item-pills-checklists-tab" data-toggle="pill" href="#item-pills-checklists" role="tab">Checklists</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" id="item-pills-notes-tab" data-toggle="pill" href="#item-pills-notes" role="tab">Notes</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" id="item-pills-info-tab" data-toggle="pill" href="#item-pills-info" role="tab">Info</a>
-          </li>
-        </ul>
+        <!-- info -->
+        <div id="item-pills-info">
 
+          <div class="row">
 
-        <!-- tab content -->
-        <div class="tab-content">
+            <div class="col-9">
 
-          <!-- checklists -->
-          <div class="tab-pane fade" id="item-pills-checklists" role="tabpanel">
-
-            <br>
-            <h1 class="custom-font">Checklists</h1><br>
-
-            <div class="row">
-              <div class="col-2">
-
-                <b>Current lists</b><br>
-                <!-- side bar of item checklists -->
-                <ul class="nav nav-pills flex-column sidebar-item-checklists">
-                </ul>
-                
-                <!-- new project checklist dropdown -->
-                <div class="dropright">
-                  <button class="btn btn-secondary btn-sm" type="button" data-toggle="dropdown">Add checklist</button>
-                  <div class="dropdown-menu">
-                    <form>
-                      <div class="form-group">
-                        <label for="new-project-checklist-name">Name:</label>
-                        <input type="text" id="new-item-checklist-name" class="form-control update-button" data-button-id="#new-item-checklist-btn">
-                      </div>
-                      <button id="new-item-checklist-btn" type="button" class="btn btn-primary float-right" disabled>Add</button>
-                    </form>
-                  </div>
+              <div class="info-section name">
+                <div class="heading">
+                  <div class="icon"><i class='bx bx-card'></i></div>
+                  <div class="description">Item name</div>
                 </div>
-
+                <div class="content"></div>
               </div>
 
-              <!-- open checklists go here -->
-              <div class="col-10" id="item-checklists">
+              <div class="info-section date-due">
+                <div class="heading">
+                  <div class="icon"><i class='bx bx-time'></i></div>
+                  <div class="description">Date due</div>
+                </div>
+                <div class="content"></div>
               </div>
+
+              <div class="info-section description">
+                <div class="heading">
+                  <div class="icon"><i class='bx bx-detail'></i></div>
+                  <div class="description">Description</div>
+                </div>
+                <div class="content"></div>
+              </div>
+
             </div>
-          </div>
 
+            <div class="col-3">
+              <h6>Actions</h6>
 
-          <!-- notes -->
-          <div class="tab-pane fade" id="item-pills-notes" role="tabpanel">
-            <h1 class="custom-font">Notes</h1>
-
-            <!-- new note input -->
-            <form>
-              <div class="form-group">
-                <!-- <label for="new-item-note-input">New note:</label> -->
-                <textarea id="new-item-note-input" class="form-control update-button" data-button-id="#new-item-note-btn" rows="5" placeholder="Write a note..."></textarea>
-              </div>
-
-              <button type="button" class="btn btn-primary" id="new-item-note-btn" onclick="addItemNote()" disabled>Save</button>
-            </form>
-
-            <!-- item note cards -->
-            <div id="item-notes-cards"></div>
-          </div>
-
-          
-          <!-- info -->
-          <div class="tab-pane fade show active" id="item-pills-info" role="tabpanel">
-
-            <div class="row">
-
-              <div class="col-9">
-
-                <div class="info-section name">
-                  <div class="heading">
-                    <div class="icon"><i class='bx bx-card'></i></div>
-                    <div class="description">Item name</div>
-                  </div>
-                  <div class="content"></div>
-                </div>
-
-                <div class="info-section date-due">
-                  <div class="heading">
-                    <div class="icon"><i class='bx bx-time'></i></div>
-                    <div class="description">Date due</div>
-                  </div>
-                  <div class="content"></div>
-                </div>
-
-                <div class="info-section description">
-                  <div class="heading">
-                    <div class="icon"><i class='bx bx-detail'></i></div>
-                    <div class="description">Description</div>
-                  </div>
-                  <div class="content"></div>
-                </div>
-
-              </div>
-
-              <div class="col-3">
-                <h6>Actions</h6>
-
-                <div class="list-group list-group-flush">
-                  
-                  <!-- set item to complete or incomplete -->
-                  <button type="button" class="list-group-item list-group-item-action" id="complete-item-modal-button" onclick="updateItemCompleted()"></button>
-                  
-                  <!-- edit item info button -->
-                  <div class="dropleft">
-                    <button type="button" class="list-group-item list-group-item-action" data-toggle="dropdown">
-                      <i class='bx bx-pencil'></i>&nbsp;Edit
-                    </button>
-                    <div class="dropdown-menu dropdown-form" id="edit-item-info-menu">
-                      <h5>Edit item info</h5>
-                      
-                      <!-- name -->
-                      <div class="form-group">
-                        <label for="edit-item-name">Name:</label>
-                        <input type="text" id="edit-item-name" class="form-control update-button" data-button-id="#edit-item-description-btn">
-                      </div>
-                      
-                      <!-- date due -->
-                      <div class="form-group">
-                        <label for="edit-item-date-due">Date due:</label>
-                        <input type="text" id="edit-item-date-due" class="form-control">
-                      </div>
-
-                      <!-- date created -->
-                      <div class="form-group">
-                        <label for="edit-item-date-created">Date created:</label>
-                        <input type="text" id="edit-item-date-created" class="form-control">
-                      </div>
-
-                      <!-- description -->
-                      <div class="form-group">
-                        <label for="edit-item-description">Description:</label>
-                        <textarea id="edit-item-description" class="form-control" rows="5"></textarea>
-                      </div>
-                      
-                      <!-- update item info button -->
-                      <button type="button" class="btn btn-primary float-right" id="edit-item-description-btn" onclick="updateItemInfo()">Save</button>
-                    </div>
-                  </div>
-                  
-                  <!-- delete item button -->
-                  <button type="button" class="list-group-item list-group-item-action" onclick="deleteItem()">
-                    <i class='bx bx-trash'></i>&nbsp;Delete
+              <div class="list-group list-group-flush">
+                
+                <!-- set item to complete or incomplete -->
+                <button type="button" class="list-group-item list-group-item-action" id="complete-item-modal-button" onclick="updateItemCompleted()"></button>
+                
+                <!-- edit item info button -->
+                <div class="dropleft">
+                  <button type="button" class="list-group-item list-group-item-action" data-toggle="dropdown">
+                    <i class='bx bx-pencil'></i>&nbsp;Edit
                   </button>
+                  <div class="dropdown-menu dropdown-form" id="edit-item-info-menu">
+                    <h5>Edit item info</h5>
+                    
+                    <!-- name -->
+                    <div class="form-group">
+                      <label for="edit-item-name">Name:</label>
+                      <input type="text" id="edit-item-name" class="form-control update-button" data-button-id="#edit-item-description-btn">
+                    </div>
+                    
+                    <!-- date due -->
+                    <div class="form-group">
+                      <label for="edit-item-date-due">Date due:</label>
+                      <input type="text" id="edit-item-date-due" class="form-control">
+                    </div>
 
+                    <!-- date created -->
+                    <div class="form-group">
+                      <label for="edit-item-date-created">Date created:</label>
+                      <input type="text" id="edit-item-date-created" class="form-control">
+                    </div>
 
+                    <!-- description -->
+                    <div class="form-group">
+                      <label for="edit-item-description">Description:</label>
+                      <textarea id="edit-item-description" class="form-control" rows="5"></textarea>
+                    </div>
+                    
+                    <!-- update item info button -->
+                    <button type="button" class="btn btn-primary float-right" id="edit-item-description-btn" onclick="updateItemInfo()">Save</button>
+                  </div>
                 </div>
-              </div>
+                
+                <!-- delete item button -->
+                <button type="button" class="list-group-item list-group-item-action" onclick="deleteItem()">
+                  <i class='bx bx-trash'></i>&nbsp;Delete
+                </button>
 
+
+              </div>
             </div>
 
           </div>
 
         </div>
+
+        <!-- checklists -->
+        <div id="item-pills-checklists">
+
+          <br>
+          <h1 class="custom-font">Checklists</h1><br>
+
+          <div class="row">
+            <div class="col-2">
+
+              <b>Current lists</b><br>
+              <!-- side bar of item checklists -->
+              <ul class="nav nav-pills flex-column sidebar-item-checklists">
+              </ul>
+              
+              <!-- new project checklist dropdown -->
+              <div class="dropright">
+                <button class="btn btn-secondary btn-sm" type="button" data-toggle="dropdown">Add checklist</button>
+                <div class="dropdown-menu">
+                  <form>
+                    <div class="form-group">
+                      <label for="new-project-checklist-name">Name:</label>
+                      <input type="text" id="new-item-checklist-name" class="form-control update-button" data-button-id="#new-item-checklist-btn">
+                    </div>
+                    <button id="new-item-checklist-btn" type="button" class="btn btn-primary float-right" disabled>Add</button>
+                  </form>
+                </div>
+              </div>
+
+            </div>
+
+            <!-- open checklists go here -->
+            <div class="col-10" id="item-checklists">
+            </div>
+          </div>
+        </div>
+
+
+        <!-- notes -->
+        <div id="item-pills-notes">
+          <h1 class="custom-font">Notes</h1>
+
+          <!-- new note input -->
+          <form>
+            <div class="form-group">
+              <!-- <label for="new-item-note-input">New note:</label> -->
+              <textarea id="new-item-note-input" class="form-control update-button" data-button-id="#new-item-note-btn" rows="5" placeholder="Write a note..."></textarea>
+            </div>
+
+            <button type="button" class="btn btn-primary" id="new-item-note-btn" onclick="addItemNote()" disabled>Save</button>
+          </form>
+
+          <!-- item note cards -->
+          <div id="item-notes-cards"></div>
+        </div>
+
+        
+
+
+        
 
       </div>
       

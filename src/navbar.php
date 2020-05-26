9d84928133399e741c51ec2bb2f3b43618d115ea
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-05-26 15:58:50 -0500

    add test page

--- src/navbar.php
+++ src/navbar.php
@@ -8,6 +8,7 @@
       <ul class="navbar-nav">
         <li class="nav-item"><a class="nav-link" id="nav-item-home" href="index.php">Home</a></li>
         <li class="nav-item"><a class="nav-link" id="nav-item-new-project" href="new-project.php">New Project</a></li>
+        <li class="nav-item"><a class="nav-link" id="nav-item-test" href="test.php">Test Page</a></li>
       </ul>
     </div>
   </nav>
---
+++ src/test.php
@@ -0,0 +1,30 @@
+<?php
+include('functions.php'); 
+include('session.php'); 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include('header.php'); ?>
+  <title>Test</title>
+</head>
+<body>
+  <?php include('navbar.php'); ?>
+
+  <div class="container">
+
+    <h1 class="custom-font">Test Page</h1>
+
+    
+    
+
+
+
+
+  </div>
+
+  <?php include('footer.php'); ?>
+
+</body>
+</html>

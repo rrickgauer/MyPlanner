724f1e2226da6371dc36987882f1faddd403a1f4
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-05-28 14:19:26 -0500

    css

--- src/index.php
+++ src/index.php
@@ -20,45 +20,51 @@ $user = getUserInfo($_SESSION['userID'])->fetch(PDO::FETCH_ASSOC);
   
   <!-- project selection -->
   <div id="projects">
-    <h5 class="mb-3 mt-3">Your projects</h5>
 
-    <input type="text" class="form-control mb-3" id="project-search-input" placeholder="Search for a project...">
-    
-    <!-- project sort options dropdown -->
-    <div class="dropdown">
-      <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">Sort projects</button>
-      <div class="dropdown-menu">
+    <div class="split toolbar">
 
-        <h6 class="dropdown-header">Name</h6>
-        <button class="dropdown-item project-sorting-option active" type="button" data-project-sorting="name_asc">Ascending</button>
-        <button class="dropdown-item project-sorting-option" type="button" data-project-sorting="name_desc">Descending</button>
-        <div class="dropdown-divider"></div>
+      <div class="left">
+        <h3>Your projects</h3>
+      </div>
+      
+      <div class="right">
+        <div class="d-flex">
+          <div class="buttons d-flex">
+            <a class="btn btn-secondary mr-2" href="new-project.php">New</a>
 
-        <h6 class="dropdown-header">Date created</h6>
-        <button class="dropdown-item project-sorting-option" type="button" data-project-sorting="date_created_old">Oldest</button>
-        <button class="dropdown-item project-sorting-option" type="button" data-project-sorting="date_created_new">Newest</button>
-        <div class="dropdown-divider"></div>
+            <!-- project sort options dropdown -->
+            <div class="dropdown mr-2">
+              <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">Sort</button>
 
-        <h6 class="dropdown-header">Date due</h6>
-        <button class="dropdown-item project-sorting-option" type="button" data-project-sorting="date_due_old">Oldest</button>
-        <button class="dropdown-item project-sorting-option" type="button" data-project-sorting="date_due_new">Newest</button>
+              <div class="dropdown-menu">
+                <h6 class="dropdown-header">Name</h6>
+                <button class="dropdown-item project-sorting-option active" type="button" data-project-sorting="name_asc">Ascending</button>
+                <button class="dropdown-item project-sorting-option" type="button" data-project-sorting="name_desc">Descending</button>
+                <div class="dropdown-divider"></div>
 
-      </div>      
+                <h6 class="dropdown-header">Date created</h6>
+                <button class="dropdown-item project-sorting-option" type="button" data-project-sorting="date_created_old">Oldest</button>
+                <button class="dropdown-item project-sorting-option" type="button" data-project-sorting="date_created_new">Newest</button>
+                <div class="dropdown-divider"></div>
+
+                <h6 class="dropdown-header">Date due</h6>
+                <button class="dropdown-item project-sorting-option" type="button" data-project-sorting="date_due_old">Oldest</button>
+                <button class="dropdown-item project-sorting-option" type="button" data-project-sorting="date_due_new">Newest</button>
+              </div>      
+            </div>
+          </div>
+
+          <!-- project search input -->
+          <input type="text" class="form-control" id="project-search-input" placeholder="Search for a project...">
+        </div>
+      </div>
+      
+
+
     </div>
-
-
-    <br><br>
-
-
 
     <!-- project cards go here -->
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-      
-      <!-- add new project link -->
-<!--       <div class="col">
-        <a class="btn btn-lg btn-primary" href="new-project.php" id="new-project-btn">New project</a>
-      </div> -->
-    </div>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3"></div>
   </div>
 
 
